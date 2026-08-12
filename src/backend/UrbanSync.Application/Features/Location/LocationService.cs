eefdeeaa68bf892e7e9002c846e02d9b171d14a6
Rafael David Rivas Abreu <rrivas@elcatador.com>
2026-08-12 00:33:09 -0400

    fix: import wasn't set on location service

--- src/backend/UrbanSync.Application/Features/Location/LocationService.cs
+++ src/backend/UrbanSync.Application/Features/Location/LocationService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UrbanSync.Application.Common.Interfaces.Persistence;
 
 namespace UrbanSync.Application.Features.Location
 {
